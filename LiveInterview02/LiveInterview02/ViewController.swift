de41284fb101c2fe1a1f49ff1661919705ff59e1
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2022-01-26 12:09:49 +0100

    Add new uikit project

---
+++ LiveInterview02/LiveInterview02/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  LiveInterview02
+//
+//  Created by Matthias Zarzecki on 26.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
