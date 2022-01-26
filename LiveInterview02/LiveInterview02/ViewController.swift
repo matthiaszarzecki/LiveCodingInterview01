2c3a90435c2a7a170dd246c35d4efea3e25ad395
Matthias Zarzecki <matthias.zarzecki@gmail.com>
2022-01-26 12:58:49 +0100

    Add tableView init

--- LiveInterview02/LiveInterview02/ViewController.swift
+++ LiveInterview02/LiveInterview02/ViewController.swift
@@ -9,17 +9,45 @@ import UIKit
 
 class ViewController: UITableViewController {
   private let api = Api()
+  private var fruits: [Fruit] = []
 
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    tableView.delegate = self
+    tableView.dataSource = self
+
+    getFruits()
+  }
+
+  private func getFruits() {
     api.getFruits { result in
       switch result {
       case .success(let fruitResponse):
+        self.fruits = fruitResponse.fruits
         print(fruitResponse.fruits.count)
       case .failure(let error):
         print("Error: \(error)")
       }
     }
+  }
+
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    1
+  }
+
+  override func tableView(
+    _ tableView: UITableView,
+    numberOfRowsInSection section: Int
+  ) -> Int {
+    fruits.count
+  }
+
+  override func tableView(
+    _ tableView: UITableView,
+    cellForRowAt indexPath: IndexPath
+  ) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+    return cell
   }
 }
